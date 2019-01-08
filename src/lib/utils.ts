27c3308f465d564c8844e91fe6dcb76463eb81f3
Lam Nhan <lamnhan92@gmail.com>
2019-01-08 08:12:07 +0700

    remove makePassword

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,11 +4,6 @@ export function sha256(input: string) {
     return KJUR.crypto.Util.sha256(input);
 }
 
-export function securePassword(password: string) {
-    // TODO: implement bcrypt
-    return sha256(password);
-}
-
 export function validEmail(email: string) {
     // tslint:disable-next-line:max-line-length
     return (/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i).test(email);
